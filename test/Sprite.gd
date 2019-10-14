c6a38d140c161f09590499bf3ef6217e71936a1a
romeosierra131313 <romeosierra13@gmail.com>
2019-10-14 18:57:50 +0400

    Add files via upload

---
+++ test/Sprite.gd
@@ -0,0 +1,28 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var Year = 0
+var Month = 0
+var Day = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+func _process(delta):
+	
+	pass
+	
+func _incrementYear():
+	Year += 1;
+	pass
+	
+func _incrementMonth():
+	Month +=1;
+	pass
+
+func _incrementDay():
+	Day += 1;
+	pass
